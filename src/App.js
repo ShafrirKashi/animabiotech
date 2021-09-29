3253e93e4f14b75c1dadf5c2a38ecd8ce323b2bd
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2021-09-29 20:54:27 +0300

    29.9.21 20:54

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css';
 import UserList from './Components/UserList/UserList'
 
 function App() {
+
+
+useEffect(() =>{
+
+fetch('http://localhost:8000/users')
+  .then(res => {
+    return res.json()
+  })
+  .then(data => {
+    console.log(data);
+  })
+});
+
   return (
     <div className="App">
        <UserList />
+
     </div>
   );
 }
