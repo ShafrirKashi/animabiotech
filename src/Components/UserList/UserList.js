741d742c211dba040e0d4f18b759c653a2daa475
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2021-09-29 20:10:15 +0300

    29.9.21 20:10

---
+++ src/Components/UserList/UserList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './UserList.css'
+
+function UserList() {
+    return (
+        <div className="Main">
+            <p className="Main__list">User List</p>
+        </div>
+    )
+}
+
+export default UserList
